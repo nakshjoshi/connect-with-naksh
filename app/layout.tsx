ca24295a7ee73ed47b1251503888a2795a73b3a3
nakshjoshi <nakshjoshi2004@gmail.com>
2026-03-12 23:02:58 +0530

    add vercel speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
 
         <Analytics/>
+        <SpeedInsights/>
 
         {/* Fixed Navbar */}
         <div className="fixed top-0 left-0 right-0 z-50">
